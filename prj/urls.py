d4eaabccc73d61a479c34f226416fbdaa3fad1ee
mvasyliv <vmv_gaz@yahoo.com>
2017-11-09 17:58:39 +0200

    before start order project

--- prj/urls.py
+++ prj/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns= [
-    url(r'^$', views.prj_list, name='prj_list')
+    url(r'^$', views.prj_list, name='prj_list'),
+    url(r'^prj/(?P<pk>[0-9]+)/$', views.prj_detail, name='prj_detail'),
 ]
--- prj/views.py
+++ prj/views.py
@@ -1,6 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Prject
 
 def prj_list(request):
-    projects = Prject.objects.all()
-    return render(request, 'prj/prj_list.html', {'projects' : projects})
+    projects = Prject.objects.order_by('name_prj')
+    return render(request, 'prj/prj_list.html', {'projects': projects})
+
+def prj_detail(request, pk):
+    projectdetails = get_object_or_404(Prject, pk=pk)
+    return render(request, 'prj/prj_detail.html', {'projectdetails': projectdetails})
